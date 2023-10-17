671c713e0e2c1fc39c7e6934d0bb16b212d6fc33
Vansh <gu[ptavansh67@gmail.com>
2023-10-18 01:51:55 +0530

    finished initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:yashgautam/screens/Login.dart';
-import 'package:yashgautam/screens/SignUp.dart'; // Import the Signup screen
+import 'package:yashgautam/screens/Signup.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Sample App';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      initialRoute: '/', // Set the initial route
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/', // Set the initial route to '/'
       routes: {
         '/': (context) => const LoginScreen(), // Set the LoginScreen as the initial route
-        '/signup': (context) => const SignupScreen(), // Add a route for the SignupScreen
+        '/signup': (context) => const SignupPage(),// Add a route for the SignupScreen
       },
     );
   }
